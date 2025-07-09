5cd0083831fc3718661f7526ab5104f39b9fc349
axeliparrea <141780766+axeliparrea@users.noreply.github.com>
2025-07-09 13:01:00 -0600

    10

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+
 // Add a small delay to ensure styles are loaded
 setTimeout(() => {
   createRoot(document.getElementById('root')).render(
